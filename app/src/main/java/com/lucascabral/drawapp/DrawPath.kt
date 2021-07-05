859cc224de456340185c44fc00335e5f190931f5
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-05 17:27:50 -0300

    DrawPath refatorado.

--- app/src/main/java/com/lucascabral/drawapp/DrawPath.kt
+++ app/src/main/java/com/lucascabral/drawapp/DrawPath.kt
@@ -14,27 +14,26 @@ class DrawPath @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var paint: Paint? = null
-    private var path: Path? = null
-    private var mPath: Path? = null
+    private var paint: Paint = Paint()
+    private var path: Path = Path()
     private var pathList = ArrayList<PaintPath>()
     private var undonePathList = ArrayList<PaintPath>()
-    private var mX: Float? = null
-    private var mY: Float? = null
+    private var mX: Float = 0.0f
+    private var mY: Float = 0.0f
     private var touchTolerance: Float = 4f
 
     init {
         paint = Paint()
-        paint!!.color = Color.GREEN
-        paint!!.strokeWidth = 10f
-        paint!!.style = Paint.Style.STROKE
-        paint!!.isAntiAlias = true
+        paint.color = Color.GREEN
+        paint.strokeWidth = 10f
+        paint.style = Paint.Style.STROKE
+        paint.isAntiAlias = true
     }
 
     override fun onDraw(canvas: Canvas?) {
         if (pathList.size > 0) {
             for (paintPath in pathList) {
-                canvas?.drawPath(paintPath.path, paint!!)
+                canvas?.drawPath(paintPath.path, paint)
             }
         }
     }
@@ -55,35 +54,33 @@ class DrawPath @JvmOverloads constructor(
                 touchUp()
                 invalidate()
             }
-            else -> {
-            }
         }
         invalidate()
         return true
     }
 
     private fun touchStart(xPos: Float, yPos: Float) {
-        mPath = Path()
-        val paintPath = PaintPath(mPath!!)
+        path = Path()
+        val paintPath = PaintPath(path)
         pathList.add(paintPath)
-        mPath!!.reset()
-        mPath!!.moveTo(xPos, yPos)
+        path.reset()
+        path.moveTo(xPos, yPos)
         mX = xPos
         mY = yPos
     }
 
     private fun touchMove(xPos: Float, yPos: Float) {
-        val dX: Float = abs(xPos - mX!!)
-        val dY: Float = abs(yPos - mY!!)
+        val dX: Float = abs(xPos - mX)
+        val dY: Float = abs(yPos - mY)
         if (dX >= touchTolerance || dY >= touchTolerance) {
-            mPath!!.quadTo(mX!!, mY!!, (xPos + mX!!) / 2, (yPos + mY!!) / 2)
+            path.quadTo(mX, mY, (xPos + mX) / 2, (yPos + mY) / 2)
             mX = xPos
             mY = yPos
         }
     }
 
     private fun touchUp() {
-        mPath!!.lineTo(mX!!, mY!!)
+        path.lineTo(mX, mY)
     }
 
     fun undoPath() {
